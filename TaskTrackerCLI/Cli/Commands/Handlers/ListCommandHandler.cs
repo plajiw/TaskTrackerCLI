63415404f7df0e3a7f4ccccb73d89888b5e4d074
plajiw <pabloribeiro.cont@gmail.com>
2026-03-01 18:19:15 -0300

    refactor ConsoleUi PrintTable

--- TaskTrackerCLI/Cli/Commands/Handlers/ListCommandHandler.cs
+++ TaskTrackerCLI/Cli/Commands/Handlers/ListCommandHandler.cs
@@ -1,4 +1,6 @@
-﻿using TaskTrackerCLI.Domain.Repositories;
+﻿using System.Diagnostics;
+using TaskTrackerCLI.Domain.Models.TaskItem;
+using TaskTrackerCLI.Domain.Repositories;
 
 namespace TaskTrackerCLI.Cli.Commands.Handlers
 {
@@ -21,20 +23,7 @@ namespace TaskTrackerCLI.Cli.Commands.Handlers
                 return;
             }
 
-            string columnTemplate = "{0,-5} | {1,-35} | {2,-12} | {3,-20}";
-            ConsoleUi.WriteLine(new string('-', 80), ConsoleColor.Gray);
-            ConsoleUi.WriteLine(string.Format(columnTemplate, "ID", "DESCRIPTION", "STATUS", "CREATED AT"), ConsoleColor.DarkCyan);
-            ConsoleUi.WriteLine(new string('-', 80), ConsoleColor.Gray);
-
-            taskList.ForEach(t =>
-            {
-                var displayDescription = t.Description.Length > 32 ? t.Description[..32] + "..." : t.Description;
-
-                var row = string.Format(columnTemplate, t.Id, displayDescription, t.Status, t.CreatedAt.ToString("dd/MM/yyyy HH:mm"));
-                ConsoleUi.WriteLine(row, ConsoleColor.Cyan);
-            });
-
-            ConsoleUi.WriteLine(new string('-', 80), ConsoleColor.Gray);
+            ConsoleUi.PrintTable(taskList);
         }
     }
 }
--- TaskTrackerCLI/Cli/ConsoleUi.cs
+++ TaskTrackerCLI/Cli/ConsoleUi.cs
@@ -2,6 +2,14 @@ namespace TaskTrackerCLI.Cli;
 
 public class ConsoleUi
 {
+    private const int MaxCellLength = 128;
+    private const string Ellipsis = "...";
+    private const string DateTimeFormat = "dd/MM/yyyy HH:mm";
+    private const char VerticalSeparator = '|';
+    private const char HorizontalSeparator = '-';
+    private const char Intersection = '+';
+    private const string ColumnPadding = " ";
+    
     public static void ShowWelcome()
     {
         Console.Clear();
@@ -56,7 +64,74 @@ public class ConsoleUi
 
     public static void WriteLine(string text, ConsoleColor color)
     {
-        string newLine = Environment.NewLine;
+        var newLine = Environment.NewLine;
         Write($"{text}{newLine}", color);
+    }
+
+    public static void DrawLine()
+    {
+        const char lineChar = '-';
+        const int lineSize = 80;
+        WriteLine(new string(lineChar, lineSize), ConsoleColor.Gray);
+    }
+    
+    public static void PrintTable<T>(List<T>? itemRows) where T : class
+    {
+        if (itemRows == null)
+            return;
+
+        var properties = typeof(T).GetProperties();
+        var columnWidths = properties.Select(p => p.Name.Length).ToArray();
+
+        foreach (var item in itemRows)
+        {
+            for (var i = 0; i < properties.Length; i++)
+            {
+                var stringValue = FormatValue(properties[i].GetValue(item));
+                columnWidths[i] = Math.Max(columnWidths[i], stringValue.Length);
+            }
+        }
+        
+        var columnTemplate = string.Join(string.Empty, properties.Select((p, i) => 
+            $"{ColumnPadding}{{{i},-{columnWidths[i]}}}{ColumnPadding}{VerticalSeparator}"));
+        
+        var separatorLine = string.Join(string.Empty, columnWidths.Select((w, i) => 
+            $"{HorizontalSeparator}{new string(HorizontalSeparator, w)}{HorizontalSeparator}" +
+            $"{(i == columnWidths.Length - 1 ? VerticalSeparator : Intersection)}"));
+
+        var columnHeader = properties.Select(p => p.Name.ToUpperInvariant()).ToArray<object>();
+        WriteLine(string.Format(columnTemplate, columnHeader), ConsoleColor.DarkCyan);
+        
+        WriteLine(separatorLine, ConsoleColor.DarkCyan);
+
+        foreach (var item in itemRows)
+        {
+            var rowValues = new object[properties.Length];
+            
+            for (var i = 0; i < properties.Length; i++)
+                rowValues[i] = FormatValue(properties[i].GetValue(item));
+
+            WriteLine(string.Format(columnTemplate, rowValues), ConsoleColor.Cyan);
+        }
+
+        WriteLine(separatorLine, ConsoleColor.DarkCyan);
+    }
+
+    private static string FormatValue(object? value)
+    {
+        if (value is null) 
+            return string.Empty;
+
+        string strValue;
+
+        if (value is DateTime dateValue)
+            strValue = dateValue.ToString(DateTimeFormat);
+        else
+            strValue = value.ToString() ?? string.Empty;
+
+        if (strValue.Length > MaxCellLength)
+            return strValue[..(MaxCellLength - Ellipsis.Length)] + Ellipsis;
+
+        return strValue;
     }
 }
